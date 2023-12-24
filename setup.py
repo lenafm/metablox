18253923d5670968946324196452b4f48b4e71c9
Lena Mangold <lena.mangold@cmb.hu-berlin.de>
2023-12-24 11:52:37 +0100

    add setup py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='metablox',
+      version='0.0.1',
+      description='A library for quantifying the relationship between categorical node metadata and network block '
+                  'structure.',
+      url='https://github.com/lenafm/metablox',
+      author='Lena Mangold',
+      author_email='lena.mangold@cmb.hu-berlin.org',
+      packages=find_packages(),
+      zip_safe=False)
